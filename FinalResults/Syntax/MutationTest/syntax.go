b1c2e719166a9c88d2d1dac4d0120528053328ec
Alex Melendez <alex.melendezwhite.440@my.csun.edu>
2018-12-15 21:27:50 -0800

    Syntax

---
+++ FinalResults/Syntax/MutationTest/syntax.go
@@ -0,0 +1,18 @@
+package main
+
+//func max : Generates maximum between two numbers
+
+func max(num1, num2 int) int {
+	
+	var result int
+ 
+	if (num1 > num2) {
+
+	   result = num1
+	} else {
+		 result = num2
+	}
+
+	return result 
+
+ }
